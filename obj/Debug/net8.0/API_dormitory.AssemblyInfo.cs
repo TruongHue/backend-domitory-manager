42a946e868db1c7370871168d8c19b53552ffcd8
TruongHue <thuhue20031022@gmail.com>
2025-05-06 18:36:37 +0700

    Update import file5

--- obj/Debug/net8.0/API_dormitory.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_dormitory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_dormitory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6dd7bea47bb4956a244454e2bcdf446c286fc7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a06cc99b4b503fb38a94c832ce2b8a35936327b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_dormitory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_dormitory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
